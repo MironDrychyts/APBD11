29cd6663807b7ade1d51e279ee8e1f94ca91390f
MironDrychyts <s24113@pjwstk.edu.pl>
2024-06-27 10:45:05 +0200

    commit1

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -1,35 +1,83 @@
+using System.Text;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using WebApplication4.Context;
 using WebApplication4.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers().AddJsonOptions(options =>
+public class Program
 {
-    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IPrescriptionRepository, PrescriptionRepository>();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+        });
+        builder.Services.AddScoped<IPrescriptionRepository, PrescriptionRepository>();
+        builder.Services.AddDbContext<Apbd10Context>(options => 
+            options.UseSqlServer("Name=ConnectionStrings:DefaultConnection"));
 
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(opt =>
+        {
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,   
+                ValidateAudience = true, 
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.FromMinutes(2),
+                ValidIssuer = "https://localhost:5001",
+                ValidAudience = "https://localhost:5001", 
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]))
+            };
 
-builder.Services.AddDbContext<Apbd10Context>(options => options.UseSqlServer("Name=ConnectionStrings:Default"));
+            opt.Events = new JwtBearerEvents
+            {
+                OnAuthenticationFailed = context =>
+                {
+                    if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                    {
+                        context.Response.Headers.Add("Token-expired", "true");
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        }).AddJwtBearer("IgnoreTokenExpirationScheme",opt =>
+        {
+            opt.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,   
+                ValidateAudience = true, 
+                ValidateLifetime = false,
+                ClockSkew = TimeSpan.FromMinutes(2),
+                ValidIssuer = "https://localhost:5001", 
+                ValidAudience = "https://localhost:5001", 
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"]))
+            };
+        });
+        
+        
+        var app = builder.Build();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        //Configuring the HTTP request pipeline
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
+        app.UseMiddleware<LoggingMiddleware>();
+        app.UseHttpsRedirection();
+        app.MapControllers();
+        
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
